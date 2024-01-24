e4aa8354b94510b005d08826a3bdc1df76da6737
chrisroederucdenver <47036053+chrisroederucdenver@users.noreply.github.com>
2024-01-24 14:33:47 -0700

    add files_and_dirs

---
+++ file_and_dirs.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+
+import os
+import tempfile
+import shutil
+
+DIRNAME='sample_directory'
+
+print("--- BASICS ---")
+print(os.listdir(DIRNAME))
+
+try:
+    print(os.stat(f"{DIRNAME}/nonexistant_file"))
+except:
+    print('yeah, that threw')
+print(os.stat(f"{DIRNAME}/a"))
+print(os.stat(f"{DIRNAME}/b"))
+print(os.stat(f"{DIRNAME}/nonempty_file"))
+
+
+print("\n--- COPY PLAIN FILES --")
+original_file='sample_directory/nonempty_file'
+other_file='sample_directory/copy_file'
+with open(original_file, 'r') as orig:
+    print(f"orig: {original_file} {orig}")
+    with open(other_file, 'w') as other:
+        print(f"other: {other_file} {other}")
+        shutil.copyfileobj(orig, other)
+print(os.stat(other_file))
+
+print("\n--- TEMPORARY FILES ---")
+original_file='sample_directory/nonempty_file'
+print(f"{os.stat(original_file)}\n")
+if (1):
+    with tempfile.NamedTemporaryFile() as temp:
+        print(f"tempfile type:{type(temp)} tempfile:{temp}\n")
+        with open(original_file, 'rb') as orig:
+            print(f"origfile tempfile:{orig}\n")
+            shutil.copyfileobj(orig, temp)
+            temp.flush()
+        print(temp.name)
+        print(os.stat(temp.name))
+
