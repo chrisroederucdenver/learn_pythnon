e8288f5937701991288b91e5da9f7980c64be450
chrisroederucdenver <47036053+chrisroederucdenver@users.noreply.github.com>
2024-02-20 09:29:44 -0700

    recent work

--- examine_zip.py
+++ examine_zip.py
@@ -3,18 +3,20 @@
 import zipfile
 
 ZIPFILE='sample.zip'
-CHUNK_SIZE=10
+CHUNK_SIZE=50
 
-# Standard zip stuff
+# ...the file header is like the first 50 bytes...so ignore the rest. 
+# EXCEPT, zip is smart enough to skip the haders.
 zipObj = zipfile.ZipFile(ZIPFILE)
 for component in zipObj.namelist():
-    print(component, end = "")
+    print(f"COMPONENT: {component}")
     comp_file = zipObj.open(component)
     data_chunk = comp_file.read(CHUNK_SIZE)
     file_length = len(data_chunk)
-    while(data_chunk):
-        data_chunk = comp_file.read(CHUNK_SIZE)
-        file_length += len(data_chunk)
+    print(f"  DATA: {data_chunk}")
+    #while(data_chunk):
+    #    data_chunk = comp_file.read(CHUNK_SIZE)
+    #    file_length += len(data_chunk)
     print(f" size {file_length}")
     
     
